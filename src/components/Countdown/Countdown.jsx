8f18e6faeefcaf4d330c8e6f61bb52e00d652822
Pavel <sir.buzmakoff@yandex.ru>
2025-01-16 16:02:34 +0500

    Timer now updates

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import styles from "./Countdown.module.css";
+const moment = require('moment');
 
 
-const now = new Date()
+
+
 const date = new Date("July 22, 2025 16:30:00 GMT+05:00")
 
 function getRemainingTime() {
+    const now = new Date()
     const diff = (Date.parse(date) - Date.parse(now))/1000
-    const secLeft = Math.floor(diff%60)
-    const minLeft = Math.floor((diff/60)%60)
-    const hoursLeft = Math.floor((diff/3600)%24)
-    const daysLeft = Math.floor(diff/86400%30)
-    const monthsLeft = Math.floor(diff/2592000)
-
+    const duration = moment.duration(diff, 'seconds')
+    const secLeft = duration.seconds();
+    const minLeft = duration.minutes();
+    const hoursLeft = duration.hours();
+    const daysLeft = duration.days();
+    const monthsLeft = duration.months();
 
     return ` ${monthsLeft} мес, ${daysLeft} дн, ${hoursLeft} ч, ${minLeft} мин, ${secLeft} сек`;
 
 }
 
 
+
+
+
 const Countdown = () => {
+    const [time, setTime] = useState(getRemainingTime()); // Используем time вместо getRemainingTime
+
+    const updateTime = () => {
+        setTime(getRemainingTime()); // Обновляем состояние с новым значением
+    }
+
+    useEffect(() => {
+        const interval = setInterval(updateTime, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className={styles.container}>
-           <h1>{getRemainingTime()}</h1>
+            <h1>{time}</h1> {/* Используем состояние time для отображения */}
         </div>
-    )};
+    );
+};
 
 export default Countdown;
