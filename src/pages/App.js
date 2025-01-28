44b02d2ee637bd55aaf9aa7a01f78aef5e2e2012
Pavel <sir.buzmakoff@yandex.ru>
2025-01-28 15:00:24 +0500

    Added individual greeting for each guest. Needs some adjustments

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,11 @@
-import Header from './components/Header/Header';
+import Header from '../components/Header/Header';
 import styles from './App.module.css';
-import ToGuests from './components/ToGuests/ToGuests';
-import Countdown from './components/Countdown/Countdown';
-import Outfits from './components/Outfits/Outfits';
-import SimpleSlider from './components/Carousel/Carousel';
-import Schedule from './components/Schedule/Schedule';
-import Place from './components/Place/Place';
+import ToGuests from '../components/ToGuests/ToGuests';
+import Countdown from '../components/Countdown/Countdown';
+import Outfits from '../components/Outfits/Outfits';
+import SimpleSlider from '../components/Carousel/Carousel';
+import Schedule from '../components/Schedule/Schedule';
+import Place from '../components/Place/Place';
 
 
 const imgMen = [
