9e85700c76d64b994d18f4604b8cacd4e4c98dfd
Pavel <sir.buzmakoff@yandex.ru>
2025-03-31 16:14:29 +0500

    Altered link for guestslist

--- src/index.js
+++ src/index.js
@@ -7,14 +7,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Оборачиваем приложение в BrowserRouter */}
     <BrowserRouter>
       <Routes>
-        {/* Главная страница */}
         <Route path="/" element={<App />} />
-        {/* Страница "О нас" */}
-        {/* Страница для несуществующих маршрутов */}
-        <Route path="GuestList1234" element={<GuestList />} />
+        <Route path="/GuestList1234" element={<GuestList />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
